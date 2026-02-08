cc6cf1aaa8c1956c9e9350da02d3a51c2efc67cb
Yuhi-Sato <yuhi120101@gmail.com>
2026-02-08 12:44:18 +0900

    test: integer literals

---
+++ test/test_literals.rb
@@ -0,0 +1,19 @@
+require_relative 'test_helper'
+
+class TestLiterals < YRubyTestCase
+  def test_integer_zero
+    assert_equal 0, exec("0")
+  end
+
+  def test_integer_positive
+    assert_equal 42, exec("42")
+  end
+
+  def test_integer_negative
+    assert_equal(-42, exec("-42"))
+  end
+
+  def test_integer_large
+    assert_equal 1000000, exec("1000000")
+  end
+end
