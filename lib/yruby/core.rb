f1caa75f49e9c3c8cad8298bec5e45ca571c6ac9
Yuhi-Sato <yuhi120101@gmail.com>
2026-02-08 13:19:01 +0900

    add: control frame members
    see: rb_control_frame_struct in vm_core.h

--- lib/yruby/core.rb
+++ lib/yruby/core.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class YRuby
-  ControlFrame = Struct.new(keyword_init: true)
+  ControlFrame = Struct.new(:iseq, :pc, :sp, :ep, :type, :self_value, keyword_init: true)
   ExecutionContext = Struct.new(keyword_init: true)
 end
