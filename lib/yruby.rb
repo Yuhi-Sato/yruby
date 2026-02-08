ac4f48e32a3be3b1103363e28448fb5d765f150b
Yuhi-Sato <yuhi120101@gmail.com>
2026-02-08 18:48:54 +0900

    add: pop_frame
    see: rb_vm_pop_frame in vm_insnhelper.c

--- lib/yruby.rb
+++ lib/yruby.rb
@@ -28,27 +28,32 @@ class YRuby
     inc_sp(1)
   end
 
+  def push_frame
+    cf = ControlFrame.new(iseq: nil, pc: 0, sp: 0, ep: 0, type: nil, self_value: nil)
+    self.cfp = cfp - 1
+    stack[cfp] = cf
+  end
+
+  def pop_frame
+    stack[cfp] = nil
+    self.cfp = cfp + 1
+  end
+
   private
 
   def init
     stack = Array.new(STACK_SIZE)
     @ec = ExecutionContext.new(stack:, stack_size: STACK_SIZE, cfp: STACK_SIZE)
-    push_frame(@ec)
+    push_frame
   end
 
-  def push_frame(ec)
-    cf = ControlFrame.new(iseq: nil, pc: 0, sp: 0, ep: 0, type: nil, self_value: nil)
-    ec.cfp = ec.cfp - 1
-    ec.stack[ec.cfp] = cf
-  end
-
-  def_delegators :@ec, :cfp, :stack
+  def_delegators :@ec, :cfp, :cfp=, :stack, :stack=
 
   def current_cf
     stack[cfp]
   end
 
-  def_delegators :current_cf, :sp, :sp=
+  def_delegators :current_cf, :iseq, :iseq=, :pc, :pc=, :sp, :sp=
 
   def set_sv(x)
     stack[sp] = x
