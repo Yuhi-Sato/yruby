51732bac789a2208acadb596190f9554ef8a4bd5
Yuhi-Sato <yuhi120101@gmail.com>
2026-02-08 13:20:11 +0900

    add: execution context members
    see: rb_execution_context_struct in vm_core.h

--- lib/yruby/core.rb
+++ lib/yruby/core.rb
@@ -2,5 +2,5 @@
 
 class YRuby
   ControlFrame = Struct.new(:iseq, :pc, :sp, :ep, :type, :self_value, keyword_init: true)
-  ExecutionContext = Struct.new(keyword_init: true)
+  ExecutionContext = Struct.new(:stack, :stack_size, :cfp, keyword_init: true)
 end
