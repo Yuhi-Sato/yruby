c4e5ef2b15e83cad1392ae5acec86a96ade216eb
Yuhi-Sato <yuhi120101@gmail.com>
2026-02-08 16:25:45 +0900

    add: compile_node interface
    see: pm_compile_node in prism_compile.c

--- lib/yruby/compile.rb
+++ lib/yruby/compile.rb
@@ -4,6 +4,10 @@ class YRuby
   class Compile
     class << self
       def iseq_compile_node(iseq, node)
+        compile_node(iseq, node)
+      end
+
+      def compile_node(iseq, node)
       end
     end
   end
