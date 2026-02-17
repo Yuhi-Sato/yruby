fca2e442ccf31fce4c1b3032a51d8dac8973e6da
Yuhi-Sato <yuhi120101@gmail.com>
2026-02-17 23:45:15 +0900

    add: iseq_new_method

--- lib/yruby/iseq.rb
+++ lib/yruby/iseq.rb
@@ -9,18 +9,33 @@ class YRuby
         iseq = new
 
         Compile.new.iseq_compile_node(iseq, node)
+
         iseq.emit(Insns::Leave.new)
+
+        iseq
+      end
+
+      def iseq_new_method(def_node)
+        iseq = new
+
+        Compile.new.iseq_compile_method(iseq, def_node)
+
+        iseq.emit(Insns::Leave.new)
+
+        params = def_node.parameters
+        iseq.argc = params ? params.requireds.size : 0
 
         iseq
       end
     end
 
-    attr_accessor :local_table, :local_table_size
+    attr_accessor :local_table, :local_table_size, :argc
 
     def initialize
       @iseq_encoded = []
       @local_table = []
       @local_table_size = 0
+      @argc = 0
     end
 
     def emit(insn)
